59bb2c3667fa14704a37a860b296d8f3ad38c57d
René Nyffenegger <mail@renenyffenegger.ch>
2018-12-27 15:23:22 +0100

    + changeSettings.bat

---
+++ HKEY_CURRENT_USER/Console/changeSettings.bat
@@ -0,0 +1,8 @@
+@
+@ rem QuickEdit
+@ rem --------
+@ rem    Setting it to 1 allows to copy text from the console
+@ rem    by selecting it with the mouse and then pressing
+@ rem    the enter key
+@
+@ reg add HKCU\Console /v QuickEdit /t REG_DWORD /d 1 /f
