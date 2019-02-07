aee3476bdc3186cab4ec13456aea936bcb2932e7
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2019-02-07 20:39:46 +0100

    + automatic-logon.bat

---
+++ HKEY_LOCAL_MACHINE/SOFTWARE/Microsoft/Windows NT/CurrentVersion/Winlogon/automatic-logon.bat
@@ -0,0 +1,3 @@
+@set /p password="Enter password for %USERNAME%: " 
+@reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v DefaultUserName /t REG_SZ /d %USERNAME% /f
+@reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v DefaultPassword /t REG_SZ /d %password% /f
