43f071718a122dda731a3c536e3cb51bb6583bb2
René Nyffenegger <mail@renenyffenegger.ch>
2019-02-17 09:05:04 +0100

    v -> d

--- HKEY_CLASSES_ROOT/Local Settings/Software/Microsoft/Windows/CurrentVersion/AppContainer/Storage/microsoft.microsoftedge_8wekyb3d8bbwe/MicrosoftEdge/Main/start-page.bat
+++ HKEY_CLASSES_ROOT/Local Settings/Software/Microsoft/Windows/CurrentVersion/AppContainer/Storage/microsoft.microsoftedge_8wekyb3d8bbwe/MicrosoftEdge/Main/start-page.bat
@@ -1,2 +1,2 @@
-@reg add "HKEY_CLASSES_ROOT\Local Settings\Software\Microsoft\Windows\CurrentVersion\AppContainer\Storage\microsoft.microsoftedge_8wekyb3d8bbwe\MicrosoftEdge\Main" /v HomeButtonEnabled /t REG_DWORD    /v            1  /f
-@reg add "HKEY_CLASSES_ROOT\Local Settings\Software\Microsoft\Windows\CurrentVersion\AppContainer\Storage\microsoft.microsoftedge_8wekyb3d8bbwe\MicrosoftEdge\Main" /v HomeButtonPage    /t REG_MULTI_SZ /v "about:blank" /f
+@reg add "HKEY_CLASSES_ROOT\Local Settings\Software\Microsoft\Windows\CurrentVersion\AppContainer\Storage\microsoft.microsoftedge_8wekyb3d8bbwe\MicrosoftEdge\Main" /v HomeButtonEnabled /t REG_DWORD    /d            1  /f
+@reg add "HKEY_CLASSES_ROOT\Local Settings\Software\Microsoft\Windows\CurrentVersion\AppContainer\Storage\microsoft.microsoftedge_8wekyb3d8bbwe\MicrosoftEdge\Main" /v HomeButtonPage    /t REG_MULTI_SZ /d "about:blank" /f
