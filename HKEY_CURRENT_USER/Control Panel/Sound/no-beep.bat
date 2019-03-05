e47cdbd3e974ddd5ed8fdbe183e9ef4a17cd05ad
René Nyffenegger <mail@renenyffenegger.ch>
2019-03-05 08:08:14 +0100

    + Control Panel\Sound

---
+++ HKEY_CURRENT_USER/Control Panel/Sound/no-beep.bat
@@ -0,0 +1,1 @@
+@reg add "HKEY_CURRENT_USER\Control Panel\Sound" /v Beep /t REG_SZ /d "no"  /f  > nul
---
+++ HKEY_CURRENT_USER/Control Panel/Sound/no-extendedSounds.bat
@@ -0,0 +1,2 @@
+@reg add "HKEY_CURRENT_USER\Control Panel\Sound" /v ExtendedSounds /t REG_SZ /d "no"  /f > nul
+
