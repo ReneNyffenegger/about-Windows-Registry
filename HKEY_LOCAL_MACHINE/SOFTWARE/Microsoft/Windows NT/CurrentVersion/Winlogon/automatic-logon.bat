06f6701149fd2e9fe1e14d63397a7033cd545ab6
René Nyffenegger <mail@renenyffenegger.ch>
2019-03-19 19:49:14 +0100

    Set AutoAdminLogon to 1

--- HKEY_LOCAL_MACHINE/SOFTWARE/Microsoft/Windows NT/CurrentVersion/Winlogon/automatic-logon.bat
+++ HKEY_LOCAL_MACHINE/SOFTWARE/Microsoft/Windows NT/CurrentVersion/Winlogon/automatic-logon.bat
@@ -1,3 +1,4 @@
 @set /p password="Enter password for %USERNAME%: " 
 @reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v DefaultUserName /t REG_SZ /d %USERNAME% /f
 @reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v DefaultPassword /t REG_SZ /d %password% /f
+@reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v AutoAdminLogon  /t REG_SZ /d         1  /f
