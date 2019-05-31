f5b903d8d47a0d627be6667fde8fc863e779ae35
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2019-05-31 14:00:09 +0200

    remove "

--- HKEY_CURRENT_USER/Software/Microsoft/Office/application_version/Excel/Security/enable-access-to-VBA-object-model.ps1
+++ HKEY_CURRENT_USER/Software/Microsoft/Office/application_version/Excel/Security/enable-access-to-VBA-object-model.ps1
@@ -1,4 +1,4 @@
 #
 #  TODO: adjust version
 #
-new-itemProperty -path HKCU:\Software\Microsoft\Office\16.0\\Excel\Security -name 'AccessVBOM'   -value 1
+new-itemProperty -path HKCU:\Software\Microsoft\Office\16.0\Excel\Security -name 'AccessVBOM'   -value 1
