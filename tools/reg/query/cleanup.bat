7e94ea127efb76fae4e7295cf8fbea1061e5ed7d
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2019-07-20 05:44:56 +0200

    Improve example

--- tools/reg/query/cleanup.bat
+++ tools/reg/query/cleanup.bat
@@ -1,3 +1,4 @@
+@echo off
 rem
 rem    Delete a key. reg delete will recursively delete
 rem    all keys 
--- tools/reg/query/show.bat
+++ tools/reg/query/show.bat
@@ -1,34 +1,80 @@
-rem
-rem    Show a specific key
-rem
-       reg query HKCU\Software\tq84\apple
+@echo  off
 
 
 rem
-rem    Show a key and all its subkeys and values recursively
+echo   ** Show the values and keys of a given key
 rem
-       reg query HKCU\Software\tq84       /s
+@      reg query HKCU\Software\tq84\apple
+rem
+rem    HKEY_CURRENT_USER\Software\tq84\apple
+rem        valOne    REG_SZ    foo
+rem        valThree    REG_SZ    baz
+rem    
+rem    HKEY_CURRENT_USER\Software\tq84\apple\banana
+
 
 rem
-rem    Find values whose name is apple
+echo   ** Show a key and all its subkeys and values recursively
 rem
-       reg query HKCU\Software\tq84        /s   /v apple
+@      reg query HKCU\Software\tq84       /s
+rem
+rem    HKEY_CURRENT_USER\Software\tq84
+rem        (Default)    REG_SZ
+rem    
+rem    HKEY_CURRENT_USER\Software\tq84\apple
+rem        valOne    REG_SZ    foo
+rem        valThree    REG_SZ    baz
+rem    
+rem    HKEY_CURRENT_USER\Software\tq84\apple\banana
+rem        valTwo    REG_SZ    bar
+rem        (Default)    REG_SZ    apple
+rem    
+rem    HKEY_CURRENT_USER\Software\tq84\apple\banana\cherry
+rem        (Default)    REG_SZ
+rem        apple    REG_SZ    fruit
 
 rem
-rem    Find everything with apple: keys, value-names and values.
+echo   ** Find values whose name is apple **
+rem
+@      reg query HKCU\Software\tq84        /s   /v apple
+rem
+rem    HKEY_CURRENT_USER\Software\tq84\apple\banana\cherry
+rem        apple    REG_SZ    fruit
+rem
+
+rem
+echo   ** Find everything with apple: keys, value-names and values.
 rem    /v  specifically searches for values, /f searches for
 rem    everything.
 rem
-       reg query HKCU\Software\tq84        /s   /f apple
+@      reg query HKCU\Software\tq84        /s   /f apple
 
 rem
-rem    Restrict previous query to key names only by
-rem    adding the /k flag:
+echo   ** Restrict previous query to key names only by adding the /k flag
 rem
-       reg query HKCU\Software\tq84        /s   /f apple    /k
+@      reg query HKCU\Software\tq84        /s   /f apple    /k
+rem
+rem    HKEY_CURRENT_USER\Software\tq84\apple
+rem
 
 rem
-rem    Restrict previous query to searching in data using
-rem    the /d flag:
+echo   ** Restrict previous query to searching in data using the /d flag:
 rem
-       reg query HKCU\Software\tq84        /s   /f apple    /d
+@      reg query HKCU\Software\tq84        /s   /f apple    /d
+rem
+rem HKEY_CURRENT_USER\Software\tq84\apple\banana
+rem     (Default)    REG_SZ    apple
+rem
+
+rem
+echo   ** Searching with a substring (part) of the searched item
+rem
+@      reg query HKCU\Software\tq84 /s /f ppl
+rem
+rem    HKEY_CURRENT_USER\Software\tq84\apple
+rem    
+rem    HKEY_CURRENT_USER\Software\tq84\apple\banana
+rem        (Default)    REG_SZ    apple
+rem    
+rem    HKEY_CURRENT_USER\Software\tq84\apple\banana\cherry
+rem        apple    REG_SZ    fruit
