b8f92ffc8834f9a07c6942eb880925efb4fed768
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2019-08-16 07:47:53 +0200

    + preferred-editing-language.ps1

---
+++ HKEY_CURRENT_USER/Software/Microsoft/Office/application_version/Common/LanguageResources/preferred-editing-language.ps1
@@ -0,0 +1,14 @@
+$office_version  = '16.0'
+$newPrefEditLang = 'en-US'
+
+# $regKey = 
+
+$regPath = "HKCU:\Software\Microsoft\Office\$($office_version)\Common\LanguageResources"
+$regKey  = get-itemProperty -path $regPath
+
+$prevPrefEditLang = $regKey.PreferredEditingLanguage
+
+$eatMe = new-itemProperty -path $regPath -name         PreferredEditingLanguage -propertyType string -value $newPrefEditLang  -force
+$eatMe = new-itemProperty -path $regPath -name PreviousPreferredEditingLanguage -propertyType string -value $prevPrefEditLang -force
+
+# $regKey.PreviousPreferredEditingLanguage = $prevPrefEditLang
