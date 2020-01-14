6e7bf816d7acac11fa2c1095f3f2b3c914f1eceb
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2020-01-14 07:56:53 +0100

    Done: adjusted version

--- HKEY_CURRENT_USER/Software/Microsoft/Office/application_version/Excel/Security/enable-access-to-VBA-object-model.ps1
+++ HKEY_CURRENT_USER/Software/Microsoft/Office/application_version/Excel/Security/enable-access-to-VBA-object-model.ps1
@@ -1,4 +1,11 @@
 #
-#  TODO: adjust version
+# Following value is something like 'Application.Excel.16'
 #
-new-itemProperty -path HKCU:\Software\Microsoft\Office\16.0\Excel\Security -name 'AccessVBOM'   -value 1
+$officeVersion = (get-item hklm:\Software\Classes\excel.application\curVer).getValue('')
+
+#
+# Remove 'Application.Excel.' from / add '.0' to version string, resulting in something like '16.0'
+#
+$officeVersion = $officeVersion -replace '.*\.(\d+)', '$1.0'
+
+new-itemProperty -path HKCU:\Software\Microsoft\Office\$officeVersion\Excel\Security -name 'AccessVBOM' -value 1
