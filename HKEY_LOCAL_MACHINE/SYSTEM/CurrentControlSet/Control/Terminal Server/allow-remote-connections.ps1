94d5e02bbd3b511f7e8614649d021531cb7b8bbd
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2020-04-08 20:36:06 +0200

    hklm:

--- HKEY_LOCAL_MACHINE/SYSTEM/CurrentControlSet/Control/Terminal Server/allow-remote-connections.ps1
+++ HKEY_LOCAL_MACHINE/SYSTEM/CurrentControlSet/Control/Terminal Server/allow-remote-connections.ps1
@@ -1,1 +1,1 @@
-$null = new-itemProperty -path 'hklm\SYSTEM\CurrentControlSet\Control\Terminal Server' -name fDenyTSConnections -propertyType DWord -value 0 -force
+$null = new-itemProperty -path 'hklm:\SYSTEM\CurrentControlSet\Control\Terminal Server' -name fDenyTSConnections -propertyType DWord -value 0 -force
