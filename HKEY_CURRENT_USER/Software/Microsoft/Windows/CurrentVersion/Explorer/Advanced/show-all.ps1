17882002c4e38c18b737dab1bf97d06ba4913aca
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2020-08-18 09:23:09 +0200

    + show-all.ps1

---
+++ HKEY_CURRENT_USER/Software/Microsoft/Windows/CurrentVersion/Explorer/Advanced/show-all.ps1
@@ -0,0 +1,3 @@
+$null = set-itemProperty HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced Hidden          1  #  default = 2?
+$null = set-itemProperty HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced HideFileExt     0  #  default = 1?
+$null = set-itemProperty HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced ShowSuperHidden 1  #  default = 0?
