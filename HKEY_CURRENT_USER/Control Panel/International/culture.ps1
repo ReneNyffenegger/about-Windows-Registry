f54017c87697b6da78d40669bb69076866542f71
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2020-08-22 14:43:59 +0200

    Still trying to set 24 hours format

--- HKEY_CURRENT_USER/Control Panel/International/culture.ps1
+++ HKEY_CURRENT_USER/Control Panel/International/culture.ps1
@@ -2,7 +2,8 @@ $culture = get-culture
                                                            #  Value being is changed
                                                            # --------------------------
 $culture.dateTimeFormat.shortDatePattern = 'yyyy-MM-dd'    #  sShortDate, sDate, iDate
-$culture.dateTimeFormat.shortTimePattern = 'hh:mm'         #    ???
+$culture.dateTimeFormat.shortTimePattern = 'HH:mm'         #  HH is supposed to be 24 hours, why does it not seem to be working.
+$culture.dateTimeFormat.pmDesignator     = ''              #  Don't show AM/PM
 
 $culture.numberFormat.CurrencyPositivePattern =  2         #  iCurrency
 $culture.numberFormat.CurrencySymbol          = 'EUR'      #  sCurrency
