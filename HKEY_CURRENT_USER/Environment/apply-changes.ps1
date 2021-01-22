4962dca8a50bb8de501501093ea2fe07292772bb
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2021-01-22 01:10:52 +0100

    check return value

--- HKEY_CURRENT_USER/Environment/apply-changes.ps1
+++ HKEY_CURRENT_USER/Environment/apply-changes.ps1
@@ -14,16 +14,20 @@ $funcDef = @'
 
 '@
 
-
 $funcRef = add-type -namespace WinAPI -name functions -memberDefinition $funcDef
 
 $HWND_BROADCAST   = [intPtr] 0xFFFF
 $WM_SETTINGCHANGE =          0x001A  # Same as WM_WININICHANGE
 $fuFlags          =               2  # SMTO_ABORTIFHUNG: return if receiving thread does not respond (hangs)
-$timeOutMs        =            1000  # Timeout in milli seconds 
+$timeOutMs        =            1000  # Timeout in milli seconds
 $res              = [uIntPtr]::zero
 
+# If the function succeeds, this value is non-zero.
 $funcVal = [WinAPI.functions]::SendMessageTimeout($HWND_BROADCAST, $WM_SETTINGCHANGE, [UIntPtr]::zero, "Environment", $fuFlags, $timeOutMs, [ref] $res);
 
-write-host "funcVal = $funcVal"  # If the function succeeds, this value is non-zero.
-write-host "res     = $res"
+if ($funcVal -eq 0) {
+   write-host "SendMessageTimeout did not succeed, res= $res"
+}
+else {
+   write-host "Message sent"
+}
