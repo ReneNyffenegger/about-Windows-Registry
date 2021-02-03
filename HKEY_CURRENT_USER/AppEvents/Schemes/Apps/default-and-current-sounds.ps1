781d528b4068458cb6d6d86c8afb4749ee68567c
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2021-02-03 22:10:30 +0100

    Remove default values of .Current

--- HKEY_CURRENT_USER/AppEvents/Schemes/Apps/default-and-current-sounds.ps1
+++ HKEY_CURRENT_USER/AppEvents/Schemes/Apps/default-and-current-sounds.ps1
@@ -6,18 +6,22 @@ foreach ($appName in $apps.GetSubKeyNames()) {
 
    $appName
 
-   $app = $apps.OpenSubKey($appName);
+   $app = $apps.OpenSubKey($appName)
 
    foreach ($eventName in $app.GetSubKeyNames()) {
 
       $event = $app.OpenSubKey($eventName)
 
-      $current = $event.OpenSubKey('.Current') # .GetValue('')
-      $default = $event.OpenSubKey('.Default') # .GetValue('')
+      $current = $event.OpenSubKey('.Current', $true) # Open with $true for write access
+      $default = $event.OpenSubKey('.Default'       )
 
       if ($current) { $currentVal = $current.GetValue('') } else { $currentVal = '' }
       if ($default) { $defaultVal = $default.GetValue('') } else { $defaultVal = '' }
 
       "   {0,-30} {1,-60} {2,-60}" -f $eventName, $currentVal, $defaultVal
+
+      if ($currentVal) {
+         $current.DeleteValue('')
+      }
    }
 }
