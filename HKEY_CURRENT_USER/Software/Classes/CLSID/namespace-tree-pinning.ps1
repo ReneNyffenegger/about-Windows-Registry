48a35a942da414ec5d72c3843d09ee509d8ea41c
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2021-07-15 18:27:23 +0200

    Add DropBox

--- HKEY_CURRENT_USER/Software/Classes/CLSID/namespace-tree-pinning.ps1
+++ HKEY_CURRENT_USER/Software/Classes/CLSID/namespace-tree-pinning.ps1
@@ -19,4 +19,5 @@ function set-pinning($clsid, $value) {
   set-pinning  '{018D5C66-4533-4307-9B53-224DE2ED1FE6}'  0   # OneDrive   (Value might already exist in registry)
   set-pinning  '{F02C1A0D-BE21-4350-88B0-7367FC96EF3C}'  0   # Network
 # set-pinning  '{031E4825-7B94-4dc3-B131-E946B44C8DD5}'  0   # Libraries
-# set-pinning  '{B4FB3F98-C1EA-428d-A78A-D1F5659CBA93}'  0   # Libraries
+# set-pinning  '{B4FB3F98-C1EA-428d-A78A-D1F5659CBA93}'  0   # Home group
+# set-pinning  '{E31EA727-12ED-4702-820C-4B6445F28E1A}'  0   # Drop Box
