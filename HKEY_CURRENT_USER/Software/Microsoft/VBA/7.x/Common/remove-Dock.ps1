b165cd4b90968ae6313b05f6abcb33f6e47108e4
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2021-08-22 20:36:33 +0200

    Add comments

--- HKEY_CURRENT_USER/Software/Microsoft/VBA/7.x/Common/remove-Dock.ps1
+++ HKEY_CURRENT_USER/Software/Microsoft/VBA/7.x/Common/remove-Dock.ps1
@@ -1,6 +1,12 @@
+ #
+ #  Get all keys that match 7.* …
+ #
 $reg_key_VBA_7_x =  , @(get-item HKCU:\Software\Microsoft\VBA\7.*)
 
 if ($reg_key_VBA_7_x.length -ne 1) {
+ #
+ # … and make sure that we have found only one key.
+ #
    write-host "only one key matching 7.* was expected"
    return
 }
