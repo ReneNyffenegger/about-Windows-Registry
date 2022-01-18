2f59dc663d7a0a18a3f6a0219d101112880ef3c8
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2022-01-18 10:53:57 +0100

    Modify list separator, too

--- HKEY_CURRENT_USER/Control Panel/International/culture.ps1
+++ HKEY_CURRENT_USER/Control Panel/International/culture.ps1
@@ -1,11 +1,13 @@
 $culture = get-culture
-                                                           #  Value being is changed
-                                                           # --------------------------
-$culture.dateTimeFormat.shortDatePattern = 'yyyy-MM-dd'    #  sShortDate, sDate, iDate
-$culture.dateTimeFormat.shortTimePattern = 'HH:mm'         #  HH is supposed to be 24 hours, why does it not seem to be working.
-$culture.dateTimeFormat.pmDesignator     = ''              #  Don't show AM/PM
+                                                             #  Value being is changed
+                                                             # --------------------------
+$culture.dateTimeFormat.shortDatePattern      = 'yyyy-MM-dd' #  sShortDate, sDate, iDate
+$culture.dateTimeFormat.shortTimePattern      = 'HH:mm'      #  HH is supposed to be 24 hours, why does it not seem to be working.
+$culture.dateTimeFormat.pmDesignator          = ''           #  Don't show AM/PM
 
-$culture.numberFormat.CurrencyPositivePattern =  2         #  iCurrency
-$culture.numberFormat.CurrencySymbol          = 'EUR'      #  sCurrency
+$culture.numberFormat.currencyPositivePattern =  2           #  iCurrency
+$culture.numberFormat.CurrencySymbol          = 'EUR'        #  sCurrency
+
+$culture.textInfo.listSeparator               = ','          #  sList
 
 set-culture $culture
