accd0179fe47fd7f51a5fd6d9ec4fcf51025141c
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2022-08-15 17:26:28 +0200

    add path to ...Regedit/Favorites if it does not exist

--- HKEY_CURRENT_USER/Software/Microsoft/Windows/CurrentVersion/Applets/Regedit/Favorites/add-some.ps1
+++ HKEY_CURRENT_USER/Software/Microsoft/Windows/CurrentVersion/Applets/Regedit/Favorites/add-some.ps1
@@ -1,3 +1,7 @@
+if (-not (test-path HKCU:/Software/Microsoft/Windows/CurrentVersion/Applets/Regedit/Favorites )) {
+   $null = new-item -force HKCU:/Software/Microsoft/Windows/CurrentVersion/Applets/Regedit/Favorites
+}
+
 $null = set-itemProperty HKCU:/Software/Microsoft/Windows/CurrentVersion/Applets/Regedit/Favorites -name regFavorites -value 'HKCU\Software\Microsoft\Windows\CurrentVersion\Applets\Regedit\Favorites'
 $null = set-itemProperty HKCU:/Software/Microsoft/Windows/CurrentVersion/Applets/Regedit/Favorites -name userEnv      -value 'HKCU\Environment'
 $null = set-itemProperty HKCU:/Software/Microsoft/Windows/CurrentVersion/Applets/Regedit/Favorites -name machineEnv   -value 'HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment'
