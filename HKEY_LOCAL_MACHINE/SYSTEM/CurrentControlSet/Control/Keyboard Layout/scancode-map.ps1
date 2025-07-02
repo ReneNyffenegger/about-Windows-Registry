214c4fd84b99095755dea4eb1bb3db573ecff6f1
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2025-07-02 15:12:19 +0200

    Update scancode-map.ps1

--- HKEY_LOCAL_MACHINE/SYSTEM/CurrentControlSet/Control/Keyboard Layout/scancode-map.ps1
+++ HKEY_LOCAL_MACHINE/SYSTEM/CurrentControlSet/Control/Keyboard Layout/scancode-map.ps1
@@ -1,16 +1,29 @@
+function set-scancodeMap (
+   [byte[]] $swaps
+) {
+
+  [byte] $nofSwaps = $swaps.length / 4
+
+   if ($nofSwaps -ne ($nofSwaps -as [int] )) {
+      write-host "incorrect number of swaps"
+      return
+   }
+
+  [byte[]] $mapping = 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+       ($nofSwaps+1), 0x00, 0x00, 0x00 +
+        $swaps                         + 
+                0x00, 0x00, 0x00, 0x00
+
+   $null = new-itemProperty -path "HKLM:\SYSTEM\CurrentControlSet\Control\Keyboard Layout" -name "Scancode Map" -value $mapping -force
+}
+
 $s_ESC  = 0x01, 0x00
 $s_CAPS = 0x3a, 0x00
 $s_RWIN = 0x5c, 0xe0
 $s_RCTR = 0x1d, 0xe0
 
-$mapping = [byte[]] (
-  0x00, 0x00, 0x00, 0x00,
-  0x00, 0x00, 0x00, 0x00,
-  0x04, 0x00, 0x00, 0x00+  # 3 mapping + 1 ending = 0x04
-  $s_ESC    + $s_CAPS   +  # 1st mapping: caps lock -> escape
-  $s_CAPS   + $s_ESC    +  # 2nd mapping: escape -> caps lock
-  $s_RCTR   + $s_RWIN   +  # 3rd mapping: right windows -> right control
-  0x00, 0x00, 0x00, 0x00   # final four bytes
+set-scancodeMap (
+   $s_ESC    + $s_CAPS   +  # 1st mapping: caps lock     -> escape
+   $s_CAPS   + $s_ESC    +  # 2nd mapping: escape        -> caps lock
+   $s_RCTR   + $s_RWIN      # 3rd mapping: right windows -> right control
 )
-
-$null = new-itemProperty -path "HKLM:\SYSTEM\CurrentControlSet\Control\Keyboard Layout" -name "Scancode Map" -value $mapping -force
